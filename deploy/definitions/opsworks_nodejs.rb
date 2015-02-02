ec4b705a97c1bb1a9e40095dde818c0564c39a50
Michael <michael@compsol.com.au>
2015-02-02 18:08:25 +1100

    added bouncy in it so that we can some vhosting in it.

--- deploy/definitions/opsworks_nodejs.rb
+++ deploy/definitions/opsworks_nodejs.rb
@@ -31,7 +31,8 @@ define :opsworks_nodejs do
       :deploy => deploy,
       :application_name => application,
       :meza_script_1 => "#{deploy[:deploy_to]}/current/meza-serviced.js",
-      :meza_script_2 => "#{deploy[:deploy_to]}/current/meza-wwwd.js"
+      :meza_script_2 => "#{deploy[:deploy_to]}/current/meza-wwwd.js",
+      :meza_script_3 => "#{deploy[:deploy_to]}/current/server.js"
 
     )
     notifies :restart, "service[monit]", :immediately
