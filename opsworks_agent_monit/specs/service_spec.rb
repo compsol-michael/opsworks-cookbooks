1c3b4030e77fca52e3e4678399babf9180896bbf
Michael <michael@compsol.com.au>
2015-03-05 16:27:22 +1100

    added up again service monitoring and changed default listening port 80 to 3001

--- opsworks_agent_monit/specs/service_spec.rb
+++ opsworks_agent_monit/specs/service_spec.rb
@@ -4,4 +4,7 @@ describe_recipe 'opsworks-agent-monit::service' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  it 'should install the service'
+  it 'should enable the service'
+
 end
