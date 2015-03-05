f12e958201ace82d5dd6cc13fae918603fc8f3fe
Michael <michael@compsol.com.au>
2015-03-05 15:51:20 +1100

    changed default nginx conf file and removed minitest

--- opsworks_agent_monit/specs/service_spec.rb
+++ opsworks_agent_monit/specs/service_spec.rb
@@ -4,6 +4,4 @@ describe_recipe 'opsworks-agent-monit::service' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
-  it 'should install the service'
-  it 'should enable the service'
 end
