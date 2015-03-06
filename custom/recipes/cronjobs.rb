ca74f3f5dd520b696807163e9d66fc260a4b9c69
Michael <michael@compsol.com.au>
2015-03-06 12:36:43 +1100

    modified cronjob

--- custom/recipes/cronjobs.rb
+++ custom/recipes/cronjobs.rb
@@ -3,10 +3,5 @@ cron "empty_log_files" do
   	hour "0"
   	minute "0"
   	weekday "6"
-  	command "
-  		> /var/log/nginx/error.log;
-  		> /var/log/nginx/access.log;
-  		> /var/log/nginx/meza-wwwd.log;
-  		> /var/log/nginx/meza-serviced.log;
-  	"
+  	command "> /var/log/nginx/error.log && > /var/log/nginx/access.log && > /var/log/nginx/meza-wwwd.log && > /var/log/nginx/meza-serviced.log"
 end
